50f7c4497619b9db80ec4d3e24dc1d6f2f7897d8
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-08-01 19:57:50 -0400

    initial setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,16 @@
 import { createStore } from 'vuex'
+import API_KEY from './api_key'
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    api_key: API_KEY,
+  },
+});
 
 export default createStore({
   state: {
