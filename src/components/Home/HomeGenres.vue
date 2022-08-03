2493642f42ec9ee224a21772108c5d1de14964f0
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-08-03 12:44:45 -0400

    movies genre menu fixed

--- src/components/Home/HomeGenres.vue
+++ src/components/Home/HomeGenres.vue
@@ -3,12 +3,12 @@
     <div class="card-header">Categories</div>
     <ul class="list-group list-group-flush">
       <li
-        class="list-group-item"
+        class="list-group-item list-group-item-action"
         v-for="genre in genres"
         :key="genre"
         :v-if="genres"
       >
-        <router-link :to="'/filter/'+genre.id">
+        <router-link :to="'/filter/'+genre.id" class="text-center text-decoration-none text-dark">
           {{ genre.name }}
         </router-link>
       </li>
