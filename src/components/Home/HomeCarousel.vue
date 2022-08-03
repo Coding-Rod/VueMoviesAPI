eafd0786090cf3c0cef6cf7afb264fbda33084c9
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-08-03 00:25:08 -0400

    genres modularized

--- src/components/Home/HomeCarousel.vue
+++ src/components/Home/HomeCarousel.vue
@@ -1,90 +1,88 @@
 <template>
-  <main class="col-12 col-lg-9">
-    <div class="container-fluid">
-      <div class="row mb-4 mb-lg-2">
-        <div class="col-9 text-left">
-          <h2>Trends</h2>
-        </div>
-        <div class="col-3">
-          <router-link class="btn btn-primary" to="/trends"
-            >Discover more
-          </router-link>
-        </div>
+  <div class="container-fluid">
+    <div class="row mb-4 mb-lg-2">
+      <div class="col-9 text-left">
+        <h2>Trends</h2>
       </div>
-      <div class="row">
-        <div
-          id="carouselExampleDark"
-          class="carousel carousel-dark slide"
-          data-bs-ride="carousel"
-        >
-          <div class="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="0"
-              class="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-
-          <div class="carousel-inner">
-            <div
-              class="carousel-item fill"
-              :class="index == 0 ? 'active' : ''"
-              v-for="(movie, index) in movies"
-              :key="movie"
-            >
-              <img
-                v-if="screenwidth >= 763"
-                :src="'https://image.tmdb.org/t/p/w500' + movie.backdrop_path"
-                :alt="movie.original_title"
-              />
-              <img
-                v-else
-                :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
-                :alt="movie.original_title"
-              />
-              <div class="carousel-caption movie-description">
-                <h5>{{ movie.original_title }}</h5>
-                <p>{{ movie.overview }}</p>
-              </div>
-            </div>
-          </div>
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
+      <div class="col-3">
+        <router-link class="btn btn-primary" to="/trends"
+          >Discover more
+        </router-link>
       </div>
     </div>
-  </main>
+    <div class="row">
+      <div
+        id="carouselExampleDark"
+        class="carousel carousel-dark slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+
+        <div class="carousel-inner">
+          <div
+            class="carousel-item fill"
+            :class="index == 0 ? 'active' : ''"
+            v-for="(movie, index) in movies"
+            :key="movie"
+          >
+            <img
+              v-if="screenwidth >= 763"
+              :src="'https://image.tmdb.org/t/p/w500' + movie.backdrop_path"
+              :alt="movie.original_title"
+            />
+            <img
+              v-else
+              :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
+              :alt="movie.original_title"
+            />
+            <div class="carousel-caption movie-description">
+              <h5>{{ movie.original_title }}</h5>
+              <p>{{ movie.overview }}</p>
+            </div>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -105,11 +103,11 @@ export default {
 }
 
 .fill {
-    overflow: hidden;
-    > img {
-      flex-shrink: 0;
-      min-width: 100%;
-      min-height: 100%;
-    }
+  overflow: hidden;
+  > img {
+    flex-shrink: 0;
+    min-width: 100%;
+    min-height: 100%;
+  }
 }
 </style>
---
+++ src/components/Home/HomeGenres.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="card-header">Categories</div>
+    <ul class="list-group list-group-flush">
+      <li
+        class="list-group-item"
+        v-for="genre in genres"
+        :key="genre"
+        :v-if="genres"
+      >
+        {{ genre.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        genres: {},
+    },
+};
+</script>
+
+<style>
+</style>
